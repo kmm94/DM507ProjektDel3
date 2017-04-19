0ae9b39f782171df4d083d703a2c70cdf4802277
karim møller <Karimkuku@gmail.com>
2017-04-19 14:07:07 +0200

    test

--- src/dm507projekt3/Encode.java
+++ src/dm507projekt3/Encode.java
@@ -1,10 +1,19 @@
 package dm507projekt3;
 
+import javax.management.Query;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by karim møller on 10-04-2017.
  */
 public class Encode {
-
-
+    public static void main(String[] args) throws IOException {
+        FileInputStream inFile = new FileInputStream(args[0]);
+//        FileOutputStream outFile = new FileOutputStream(args[1]);
+        System.out.println(inFile.read());
+    }
 
 }
