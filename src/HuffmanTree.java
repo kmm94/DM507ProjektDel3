8063aea045ec4a27dbf1ce90306b294c7fc755c9
karim møller <Karimkuku@gmail.com>
2017-05-06 18:28:00 +0200

    optimization

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -3,11 +3,9 @@
  */
 public class HuffmanTree {
 
-    private PQHeap pq = null;
-
 
     public Element makeHuffmanTree(int[] a) {
-        pq = new PQHeap(256);
+        PQHeap pq = new PQHeap(256);
         for (int i = 0; i < a.length; i++) {
             BinaryTree db = new BinaryTree();
             db.insert(a[i], i);
